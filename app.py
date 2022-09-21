e7bcfbcb25fd3b5309cd35022168c2886f67bfc7
adilm2245 <61876695+adilm2245@users.noreply.github.com>
2022-09-22 01:11:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 from predict_page import show_predict_page
 from explore_page import show_explore_page
-import pickle
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 
