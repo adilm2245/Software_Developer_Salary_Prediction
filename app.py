afff1e960914c7648841534e9ee8948a09d17391
adilm2245 <61876695+adilm2245@users.noreply.github.com>
2022-09-21 17:23:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from predict_page import show_predict_page
 from explore_page import show_explore_page
 import pickle
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
